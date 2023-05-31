9a4e5728a455f4c5e52eeccdd5118b8d914ca18b
BoyanK95 <boyan_k_95@abv.bg>
2023-05-31 15:48:12 +0300

    fix: add some attributes and changed a little checkbox

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -3,16 +3,28 @@ import { useState } from 'react';
 
 function App() {
     const [changeColor, setChangeColor] = useState(true);
-    const [isDisabled, setIsDisabled] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(false);
 
     return (
         <div>
             {changeColor ? (
-                <button disabled={isDisabled} onClick={() => setChangeColor(!changeColor)} style={{ background: 'red' }}>Change to blue</button>
+                <button
+                    disabled={isDisabled}
+                    onClick={() => setChangeColor(!changeColor)}
+                    style={{ background: 'red' }}
+                >
+                    Change to blue
+                </button>
             ) : (
-                <button disabled={isDisabled} onClick={() => setChangeColor(!changeColor)} style={{ background: 'blue' }}>Change to red</button>
+                <button
+                    disabled={isDisabled}
+                    onClick={() => setChangeColor(!changeColor)}
+                    style={{ background: 'blue' }}
+                >
+                    Change to red
+                </button>
             )}
-            <input type='checkbox' onClick={() => setIsDisabled(!isDisabled)}/>
+            <input type='checkbox' id='disable-btn-checkbox' defaultChecked={isDisabled} onClick={(e) => setIsDisabled(e.target.checked)} />
         </div>
     );
 }
