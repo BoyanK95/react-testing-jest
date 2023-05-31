6ddd2eec7e8c46e6cc0d3078e30d9ee8fd6e3296
BoyanK95 <boyan_k_95@abv.bg>
2023-05-31 14:21:20 +0300

    feat: add change btn color functionality and tests for it

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      <button style={{background: 'red'}}>Change to blue</button>
-    </div>
-  );
+    const [changeColor, setChangeColor] = useState(true);
+
+
+
+    return (
+        <div>
+            {changeColor ? (
+                <button onClick={() => setChangeColor(!changeColor)} style={{ background: 'red' }}>Change to blue</button>
+            ) : (
+                <button onClick={() => setChangeColor(!changeColor)} style={{ background: 'blue' }}>Change to red</button>
+            )}
+        </div>
+    );
 }
 
 export default App;
--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct intiial color', () => {
@@ -9,5 +9,14 @@ test('button has correct intiial color', () => {
 });
 
 test('button turns blue when clicked', () => {
+  render(<App />)
+  //expect the  background of the btn to be red
+  const colorBtn = screen.getByRole('button', {name: 'Change to blue'})
 
+  //click btn
+  fireEvent.click(colorBtn)
+
+  //expect the background color to be blue and text to change
+  expect(colorBtn).toHaveStyle({background: 'blue'})
+  expect(colorBtn).toHaveTextContent('Change to red')
 })
