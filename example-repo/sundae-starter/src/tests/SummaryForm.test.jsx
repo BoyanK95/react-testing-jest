cabdfef9ff96ced5e94af9cd53ea6c227d91dd5f
BoyanK95 <boyan_k_95@abv.bg>
2024-01-02 00:12:43 +0200

    refactor: use userEvent for SummaryForm test

--- example-repo/sundae-starter/src/tests/SummaryForm.test.jsx
+++ example-repo/sundae-starter/src/tests/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 import { fireEvent, render, screen } from '@testing-library/react';
 import SummaryForm from '../pages/SummaryForm';
-// import SummaryForm from '../../../../sundaes-on-demand/src/pages/summary/SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 describe('initial state', () => {
     test('checkbox should be unchecked by default', () => {
@@ -13,16 +14,18 @@ describe('initial state', () => {
         expect(confirmButton).toBeDisabled();
     });
 
-    test('Checkbox enables button on first click and disables on second click', () => {
+    test('Checkbox enables button on first click and disables on second click', async () => {
         render(<SummaryForm />);
 
         const checkbox = screen.getByRole('checkbox');
         const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-        fireEvent.click(checkbox);
+        // fireEvent.click(checkbox)
+        await userEvent.click(checkbox);
         expect(confirmButton).toBeEnabled();
 
-        fireEvent.click(checkbox)
-        expect(confirmButton).toBeDisabled()
+        // fireEvent.click(checkbox)
+        await userEvent.click(checkbox);
+        expect(confirmButton).toBeDisabled();
     });
 });
