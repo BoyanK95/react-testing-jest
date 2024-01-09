516db4cadb39e25bc2aa14132c7c5d6ee5f9ce41
BoyanK95 <boyan_k_95@abv.bg>
2024-01-10 00:43:26 +0200

    feat: write toppings test

--- example-repo/sundae-starter/src/pages/entry/tests/Options.test.jsx
+++ example-repo/sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Options from '../Options';
 
-test('displays image for each scoop from the server', async() => {
+test('displays image for each scoop from the server', async () => {
     render(<Options optionType='scoops' />);
 
     //find images
@@ -11,4 +11,14 @@ test('displays image for each scoop from the server', async() => {
     //confirm alt text of images
     const altText = scoopImages.map((el) => el.alt);
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('display image of each toping from the server', async () => {
+    render(<Options optionType='toppings'/>);
+
+    const toppings = await screen.findAllByRole('img', { name: /topping$/i });
+    expect(toppings).toHaveLength(3);
+
+    const toppingAltText = toppings.map((el) => el.alt);
+    // expect(toppingAltText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping'])
 });
